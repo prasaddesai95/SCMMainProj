fe23a27f10f5d0efd8a2aed987f595d771d3d3f7
prasaddesai95 <prasaddesai811@gmail.com>
2023-11-30 15:09:29 +0530

    new1

--- src/main/java/com/SCM/GenericUtils/BaseClass.java
+++ src/main/java/com/SCM/GenericUtils/BaseClass.java
@@ -35,7 +35,7 @@ public class BaseClass {
 	public void config_BC() throws Throwable
 	{
 		String BROWSER = fLib.readDataFromPropertyFile("browser");
-		Thread.sleep(5000);
+		Thread.sleep(15000);
 		if (BROWSER.equalsIgnoreCase("chrome")) 
 		{
 			driver = new ChromeDriver();
